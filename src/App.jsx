0e9f06b2661f0ba450acc8d2ba82ddfd05c1700d
Lawrence Tsungu <tsungulawrence@gmail.com>
2024-05-11 15:08:26 +0300

    code

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MainPage from "./pages/MainPage";
 import { useEffect, useState } from "react";
+import Loading from "./components/Loading";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -12,7 +13,7 @@ function App() {
   useEffect(() => {
     const timeout = setTimeout(() => {
       setLoading(false);
-    }, 3000); // Change the delay time as needed
+    }, 8000); // Change the delay time as needed
 
     // Cleanup function to clear the timeout
     return () => clearTimeout(timeout);
@@ -23,25 +24,15 @@ function App() {
   };
   return (
     <div className="App">
-      {/* <BrowserRouter>
-        {isMobile() ? <MobileNav /> : <Navbar />}
-        <About />
-        <Routes>
-          <Route path="/" element={<Home />}>
-            <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter> */}
-      
-       {/* {loading ? <Loading /> : <YourMainComponent />} */}
-
-      {isMobile() ? <MobileNav /> : <Navbar />}
-      <Home />
-      <MainPage />
-      
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="mpage">
+          {isMobile() ? <MobileNav /> : <Navbar />}
+          <Home />
+          <MainPage />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import 'animate.css';
+// import './Loading.css'; // Import CSS file for loading animation
+import charityLogo from '../assets/images/hftn-logo.png'; // Import your charity logo
 
 const Loading = () => {
-  return (
-    <div className='loading'></div>
-  )
-}
+  const [loadingPercentage, setLoadingPercentage] = useState(0);
 
-export default Loading
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLoadingPercentage(prevPercentage => {
+        const newPercentage = prevPercentage + 20;
+        return newPercentage >= 100 ? 100 : newPercentage;
+      });
+    }, 1000);
+
+    // Clear interval after 5 seconds
+    setTimeout(() => {
+      clearInterval(interval);
+    }, 8000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const loadingBarStyle = {
+    width: `${loadingPercentage}%`,
+    backgroundColor: loadingPercentage === 100 ? '#4CAF50' : '#2196F3',
+  };
+
+  return (
+    <div className='loading animate__animated animate__zoomIn'>
+      <img src={charityLogo} alt="Charity Logo" className="x-logo" />
+      <div className="loading-bar">
+        <div className="loading-bar-inner" style={loadingBarStyle}>
+          {loadingPercentage}%
+        </div>
+      </div>
+      {/* {loadingPercentage === 100 && <div>Loading completed!</div>} */}
+    </div>
+  );
+};
+
+export default Loading;
