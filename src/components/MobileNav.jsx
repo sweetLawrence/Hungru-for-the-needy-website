a6e463fd9fec22d8d1d8b501cfc8b6ea45fba086
Lawrence Tsungu <tsungulawrence@gmail.com>
2024-06-03 18:26:25 +0300

    code

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,60 +1,143 @@
-import React, { useEffect, useState } from "react";
-import { Navdata } from "../assets/data/Navdata";
-import { Link, animateScroll as scroll } from "react-scroll";
-import { MdMenu } from "react-icons/md";
-// import { IoMdCheckmarkCircleOutline } from "react-icons/io";
-import LOGO from "../assets/images/hftn-logo.png";
-import "../styles/navbar.css";
-import "animate.css";
+// import React, { useEffect, useState } from "react";
+// import { Navdata } from "../assets/data/Navdata";
+// import { Link, animateScroll as scroll } from "react-scroll";
+
+// import { MdMenu } from "react-icons/md";
+
+// import LOGO from "../assets/images/hftn-logo.png";
+// import "../styles/navbar.css";
+// import "animate.css";
+
+// const MobileNav = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleMobileNav = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   const [scrolled, setScrolled] = useState(false);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const scrollTop = window.pageYOffset;
+//       if (scrollTop > 100) {
+     
+//         setScrolled(true);
+//       } else {
+//         setScrolled(false);
+//       }
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+
+
+
+//   return (
+//     <div className={`container ${scrolled ? "scrolled" : ""}`}>
+//       <div className={`mobile ${isOpen ? "open" : ""}`}>
+//         <div className="logo">
+//           <img src={LOGO} alt="logo" />
+//         </div>
+//         <div
+//           className={`toggle-icon ${scrolled ? "black" : ""}`}
+//           onClick={toggleMobileNav}
+//         >
+//           <MdMenu />
+//         </div>
+//       </div>
+//       {isOpen && (
+//         <div className="mobile-nav">
+//           {/* animate__animated animate__fadeInDown */}
+//           {Navdata.map((value, key) => (
+//             <div className="row" onClick={toggleMobileNav} key={key}>
+//               {/* {value} */}
+//               <Link
+//               onClick={toggleMobileNav}
+//               key={key}
+//               activeClass="active"
+//               to={value.toLowerCase()}
+//               spy={true}
+//               smooth={true}
+//               offset={-95}
+//               duration={1500}
+//               className="row"
+//             >
+//               {value}
+//             </Link>
+//             </div>
+
+        
+//           ))}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default MobileNav;
+
+
+
+import React, { useEffect, useState } from 'react';
+import { Link as ScrollLink, Events, scrollSpy } from 'react-scroll';
+import { Navdata } from '../assets/data/Navdata';
+import { MdMenu } from 'react-icons/md';
+import LOGO from '../assets/images/hftn-logo.png';
+import '../styles/navbar.css';
 
 const MobileNav = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  const [activeItem, setActiveItem] = useState(null);
 
   const toggleMobileNav = () => {
     setIsOpen(!isOpen);
   };
 
-  const [scrolled, setScrolled] = useState(false);
-
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.pageYOffset;
       if (scrollTop > 100) {
-        // Change 100 to your desired scroll position
         setScrolled(true);
       } else {
         setScrolled(false);
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
+
+    scrollSpy.update();
+    Events.scrollEvent.register('begin', (to, element) => {
+      setActiveItem(to);
+    });
 
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
+      Events.scrollEvent.remove('begin');
     };
   }, []);
 
   return (
-    <div className={`container ${scrolled ? "scrolled" : ""}`}>
-      <div className={`mobile ${isOpen ? "open" : ""}`}>
+    <div className={`container ${scrolled ? 'scrolled' : ''}`}>
+      <div className={`mobile ${isOpen ? 'open' : ''}`}>
         <div className="logo">
           <img src={LOGO} alt="logo" />
         </div>
         <div
-          className={`toggle-icon ${scrolled ? "black" : ""}`}
-          onClick={toggleMobileNav}
-        >
+          className={`toggle-icon ${scrolled ? 'black' : ''}`}
+          onClick={toggleMobileNav}>
           <MdMenu />
         </div>
       </div>
       {isOpen && (
         <div className="mobile-nav">
-          {/* animate__animated animate__fadeInDown */}
           {Navdata.map((value, key) => (
-            <div className="row" onClick={toggleMobileNav} key={key}>
-              {/* {value} */}
-              <Link
-              onClick={toggleMobileNav}
+            <ScrollLink
               key={key}
               activeClass="active"
               to={value.toLowerCase()}
@@ -62,13 +145,13 @@ const MobileNav = () => {
               smooth={true}
               offset={-95}
               duration={1500}
-              className="row"
+              onClick={toggleMobileNav}
             >
-              {value}
-            </Link>
-            </div>
-
-        
+              <div
+                className={`row ${activeItem === value.toLowerCase() ? 'active' : ''}`}>
+                {value}
+              </div>
+            </ScrollLink>
           ))}
         </div>
       )}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,116 @@
-import React, { useEffect, useState } from "react";
-import { Link, animateScroll as scroll } from "react-scroll";
+// import React, { useEffect, useState } from "react";
+// import { Link, animateScroll as scroll ,Element, scrollSpy, Events  } from "react-scroll";
 
-import "../styles/navbar.css";
-import { Navdata } from "../assets/data/Navdata";
-import LOGO from "../assets/images/hftn-logo.png";
+// import "../styles/navbar.css";
+// import { Navdata } from "../assets/data/Navdata";
+// import LOGO from "../assets/images/hftn-logo.png";
+
+// const Navbar = () => {
+//   const [scrolled, setScrolled] = useState(false);
+
+//   const bottomTextStyle = {
+//     color: scrolled ? "#000" : "#fff",
+//   };
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const scrollTop = window.pageYOffset;
+//       if (scrollTop > 100) {
+//         setScrolled(true);
+//       } else {
+//         setScrolled(false);
+//       }
+//     };
+
+//     window.addEventListener("scroll", handleScroll);
+
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     scrollSpy.update();
+
+//     Events.scrollEvent.register('begin', (to, element) => {
+//         setActiveItem(to);
+//     });
+
+//     return () => {
+//         Events.scrollEvent.remove('begin');
+//     };
+// }, []);
+
+//   return (
+//     <div className={`navbar ${scrolled ? "scrolled" : ""}`}>
+//       <div className="logo">
+//         <div className="image">
+//           <img src={LOGO} alt="logo" loading="lazy"/>
+//         </div>
+//         <div className="name">
+//           <span className="top">Hungry For</span>
+//           <span className="bottom" style={bottomTextStyle}>
+//             The Needy
+//           </span>
+//         </div>
+//       </div>
+
+//       <div className="content">
+//         {Navdata.map((value, key) => (
+//           // <div className="row" key={key}>
+//           //   {value}
+//           // </div>
+
+//           <Link
+//             key={key}
+//             activeClass="active"
+//             to={value.toLowerCase()}
+//             spy={true}
+//             smooth={true}
+//             offset={-100}
+//             duration={500}
+//             className="row"
+//           >
+//             {value}
+//           </Link>
+//         ))}
+//       </div>
+//       <div className="donate">
+//         <Link
+//           to="donate"
+//           spy={true}
+//           smooth={true}
+//           offset={-100}
+//           duration={900}
+//           className="donate-button"
+//         >
+//           <button>Donate</button>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+
+
+
+
+
+
+import React, { useEffect, useState } from 'react';
+import { Link, scrollSpy, Events, animateScroll as scroll } from 'react-scroll';
+import { Navdata } from '../assets/data/Navdata';
+import LOGO from '../assets/images/hftn-logo.png';
+import '../styles/navbar.css';
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [activeItem, setActiveItem] = useState(null); // Define activeItem state
 
   const bottomTextStyle = {
-    color: scrolled ? "#000" : "#fff",
+    color: scrolled ? '#000' : '#fff',
   };
 
   useEffect(() => {
@@ -22,18 +123,25 @@ const Navbar = () => {
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll);
+
+    scrollSpy.update();
+
+    Events.scrollEvent.register('begin', (to, element) => {
+      setActiveItem(to); // Set activeItem when scrolling begins
+    });
 
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener('scroll', handleScroll);
+      Events.scrollEvent.remove('begin');
     };
   }, []);
 
   return (
-    <div className={`navbar ${scrolled ? "scrolled" : ""}`}>
+    <div className={`navbar ${scrolled ? 'scrolled' : ''}`}>
       <div className="logo">
         <div className="image">
-          <img src={LOGO} alt="logo" loading="lazy"/>
+          <img src={LOGO} alt="logo" loading="lazy" />
         </div>
         <div className="name">
           <span className="top">Hungry For</span>
@@ -45,10 +153,6 @@ const Navbar = () => {
 
       <div className="content">
         {Navdata.map((value, key) => (
-          // <div className="row" key={key}>
-          //   {value}
-          // </div>
-
           <Link
             key={key}
             activeClass="active"
@@ -57,12 +161,13 @@ const Navbar = () => {
             smooth={true}
             offset={-100}
             duration={500}
-            className="row"
+            className={`row ${activeItem === value.toLowerCase() ? 'active' : ''}`}
           >
             {value}
           </Link>
         ))}
       </div>
+
       <div className="donate">
         <Link
           to="donate"
